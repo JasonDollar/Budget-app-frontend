d83f40cfa6b3be406eeb99d03e94344df1b560f6
JasonDollar <elrey33@gmail.com>
2021-03-08 18:02:03 +0100

    ExpenseForm container

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+// import axios from 'axios'
+
+import { addExpense } from '../store/actions/expenses'
+
+const ExpenseForm = ({ titleExpense = '', descriptionExpense = '', amountExpense = '', newExpense }) => {
+  const dispatch = useDispatch()
+  const [title, setTitle] = useState(titleExpense)
+  const [description, setDescription] = useState(descriptionExpense)
+  const [amount, setAmount] = useState(amountExpense)
+  const formHandler = async e => {
+    e.preventDefault()
+    if (newExpense) {
+      dispatch(addExpense({title, description, amount}))
+      return
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={formHandler}>
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+        <input type="text" value={description} onChange={e => setDescription(e.target.value)}/>
+        <input type="number" value={amount} onChange={e => setAmount(e.target.value)}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default ExpenseForm
--- src/pages/addExpense.js
+++ src/pages/addExpense.js
@@ -1,28 +1,11 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-// import axios from 'axios'
-
-import { addExpense } from '../store/actions/expenses'
+import React from 'react'
+import ExpenseForm from '../components/ExpenseForm'
 
 const AddExpensePage = () => {
-  const dispatch = useDispatch()
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [amount, setAmount] = useState(0)
-  const formHandler = async e => {
-    e.preventDefault()
-    dispatch(addExpense({title, description, amount}))
-    // const res = await axios.post('http://localhost:3080/api/v1/expenses', {title, description, amount})
-    // console.log(res.data)
-  }
+
   return (
     <div>
-      <form onSubmit={formHandler}>
-        <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
-        <input type="text" value={description} onChange={e => setDescription(e.target.value)}/>
-        <input type="number" value={amount} onChange={e => setAmount(e.target.value)}/>
-        <button type="submit">Submit</button>
-      </form>
+      <ExpenseForm />
     </div>
   )
 }
