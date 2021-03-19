3bc107571952194ad5c4c1fee22439d544254046
JasonDollar <elrey33@gmail.com>
2021-03-19 18:42:34 +0100

    fixes in CurrencySettings

--- src/components/UserSetings/CurrencySettings.js
+++ src/components/UserSetings/CurrencySettings.js
@@ -5,19 +5,25 @@ import { availableCurrencies } from '../../config/config'
 import { changeCurrency } from '../../store/actions/user'
 
 const CurrencySettings = ({ userCurrency }) => {
-  const [currency, setCurrency] = useState(userCurrency)
+  const [currency, setCurrency] = useState()
   const dispatch = useDispatch()
+
+  const changeCurrencyHandler = () => {
+    if (!currency) { return }
+
+    dispatch(changeCurrency(currency))
+  }
   // console.log(userCurrency, currency)
   return (
     <div>
       <label htmlFor="currency">Choose currency:</label>
       <select name="currency" id="currency" value={currency} onChange={e => setCurrency(e.target.value)} >
-        {/* <option value="">Choose currency</option> */}
+        <option value="">Choose currency</option>
         {availableCurrencies.map(item => (
-          <option key={item} value={item} selected={userCurrency === currency}>{item}</option>
+          <option key={item} value={item} >{item}</option>
         ))}
       </select>
-      <button disabled={userCurrency === currency} onClick={() => dispatch(changeCurrency(currency))}>Save</button>
+      <button disabled={userCurrency === currency || !currency} onClick={changeCurrencyHandler}>Save</button>
     </div>
   )
 }
