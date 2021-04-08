8f5c6d5fc0541932fbf6153598cd953caeeba511
JasonDollar <elrey33@gmail.com>
2021-04-08 20:36:40 +0200

    further improvements to Notifications

--- src/components/NotificationItem.js
+++ src/components/NotificationItem.js
@@ -1,38 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
+import { dismissNotification } from '../store/actions/ui'
+
 const ItemDiv = styled.div`
-position: relative;
-    padding: 1rem;
-    border: 1px solid black;
-    margin-bottom: 3px;
+  font-size: 1.4rem;
+  position: relative;
+  border: 1px solid rgba(200,200,200, .6);
+  border-radius: 7px;
+  margin-bottom: 3px;
+  box-shadow: ${props => props.theme.boxShadow};
+  overflow: hidden;
+  display: flex;
+  align-items: center;
 
-    &::after {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      content: '';
-      width: ${props => props.width}%;
-      height: 3px;
-      background: red;
-      /* transition: width 3s; */
-    }`
+  span {
+    display: block;
+    padding: .8rem 1rem;
+  }
 
-const NotificationItem = ({ message }) => {
-  // const width = useRef(100)
+  &::after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    content: '';
+    width: ${props => props.width}%;
+    height: 3px;
+    background: ${props => props.theme.mainThemeColor};
+  }
+  button {
+    margin: 0 .3rem;
+    margin-left: 0;
+    border: none;
+    background: none;
+  }
+`
+
+const NotificationItem = ({ message, notificationId }) => {
+  const dispatch = useDispatch()
   const [width, setWidth] = useState(100)
   useEffect(() => {
     const timer = setInterval(() => {
-      // width.current = width.current - 1
-      setWidth(state => state - 2)
-      console.log(width)
-    }, 80)
+      setWidth(state => state - 1)
+    }, 40)
     return () => {
       clearInterval(timer)
     }
   }, [])
+
   return (
-    <ItemDiv width={width}>{message}</ItemDiv>
+    <ItemDiv width={width}>
+      <span>{message}</span>
+      <button onClick={() => dispatch(dismissNotification(notificationId))}>X</button>
+    </ItemDiv>
   )
 }
 
--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import NotificationItem from './NotificationItem'
 
@@ -6,16 +6,13 @@ const Noti = styled.div`
   position: fixed;
   left: 1rem;
   bottom: 1rem;
-
 `
 
 const Notifications = ({ notifications }) => {
-
   return (
     <Noti>
       {notifications.map(item => (
-        <NotificationItem key={item.id} message={item.message}/>
-        
+        <NotificationItem key={item.id} notificationId={item.id} message={item.message}/>
       ))}
     </Noti>
   )
